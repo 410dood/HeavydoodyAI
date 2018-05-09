89665b7bbcce10f218fd31edc3fc60614bea41a9
Bill Doody <wdoody@yahoo.com>
2018-05-09 15:39:33 -0600

    moved layout to bottom navigation

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
-
 import {createBottomTabNavigator} from 'react-navigation';
 
 class HomeScreen extends React.Component {
