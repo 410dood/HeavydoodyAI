fd9e64afc1905e022632a51770d2005949c45a51
Bill Doody <wdoody@yahoo.com>
2018-05-09 15:37:47 -0600

    added navigation

--- app/index.js
+++ app/index.js
@@ -1,36 +1,38 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {StackNavigator} from 'react-navigation'
+import {StyleSheet, Text, View, Button} from 'react-native';
 
-class HeavyDoodyAI extends Component {
+import {createBottomTabNavigator} from 'react-navigation';
+
+class HomeScreen extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
+            <View
+                style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Text>Home!</Text>
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5
+class SettingsScreen extends React.Component {
+    render() {
+        return (
+            <View
+                style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Text>Settings!</Text>
+            </View>
+        );
     }
-});
+}
 
-export default HeavyDoodyAI;
+export default createBottomTabNavigator({Home: HomeScreen, Settings: SettingsScreen});
+
+// export default HeavyDoodyAI;
